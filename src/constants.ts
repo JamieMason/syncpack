b90dae4e156e9e7bfe9dd816be440aaf51b6ac0b
Jamie Mason <jamie@foldleft.io>
2018-01-30 16:53:43 +0000

    refactor(core): use default glob pattern if none provided

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 import { IManifestKey } from './typings';
 
 export const DEFAULT_PATTERN = './packages/*/package.json';
-export const DEFAULT_SOURCE = './package.json';
 export const DEPENDENCY_TYPES: IManifestKey[] = ['dependencies', 'devDependencies', 'peerDependencies'];
 export const GREATER = 1;
 export const LESSER = -1;
--- src/manifests/index.ts
+++ src/manifests/index.ts
@@ -1,3 +1,4 @@
+import { DEFAULT_PATTERN } from '../constants';
 import { IDictionary, IManifest, IManifestDescriptor } from '../typings';
 import { getManifests } from './get-manifests';
 import { manifestData } from './manifest-data';
@@ -10,27 +11,27 @@ export type SetVersionsToNewestMismatch = (pattern: string) => Promise<IManifest
 
 const unwrap = (descriptors: IManifestDescriptor[]) => descriptors.map((descriptor) => descriptor.data);
 
-export const getVersions: GetVersions = (pattern) =>
+export const getVersions: GetVersions = (pattern = DEFAULT_PATTERN) =>
   getManifests(pattern)
     .then(unwrap)
     .then(manifestData.getVersions);
 
-export const getMismatchedVersions: GetMismatchedVersions = (pattern) =>
+export const getMismatchedVersions: GetMismatchedVersions = (pattern = DEFAULT_PATTERN) =>
   getManifests(pattern)
     .then(unwrap)
     .then(manifestData.getMismatchedVersions);
 
-export const setVersion: SetVersion = (name, version, pattern) =>
+export const setVersion: SetVersion = (name, version, pattern = DEFAULT_PATTERN) =>
   getManifests(pattern)
     .then(unwrap)
     .then(manifestData.setVersion.bind(null, name, version));
 
-export const setVersionRange: SetVersionRange = (range, pattern) =>
+export const setVersionRange: SetVersionRange = (range, pattern = DEFAULT_PATTERN) =>
   getManifests(pattern)
     .then(unwrap)
     .then(manifestData.setVersionRange.bind(null, range));
 
-export const setVersionsToNewestMismatch: SetVersionsToNewestMismatch = (pattern) =>
+export const setVersionsToNewestMismatch: SetVersionsToNewestMismatch = (pattern = DEFAULT_PATTERN) =>
   getManifests(pattern)
     .then(unwrap)
     .then(manifestData.setVersionsToNewestMismatch);
