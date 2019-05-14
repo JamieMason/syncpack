2e6fe624dacb9fed57b77f4b35256add5fd2d410
Marais Rossouw <me@maraisr.com>
2019-05-14 12:12:58 +1000

    feat(lib): GetPackages to support yarn workspaces
    resolves #20

--- src/lib/get-packages.ts
+++ src/lib/get-packages.ts
@@ -8,12 +8,8 @@ export const getSources = (program: CommanderApi): string[] => {
   if (program.source && program.source.length) {
     return program.source;
   }
-  const lernaPath = resolve(process.cwd(), 'lerna.json');
-  const lerna = readJsonSync(lernaPath, { throws: false });
-  if (lerna && lerna.packages && lerna.packages.length) {
-    return lerna.packages.map((glob: string) => join(glob, 'package.json'));
-  }
-  return OPTION_SOURCES.default;
+
+  return getYarnWorkspaces() || getLernaPackages() || OPTION_SOURCES.default;
 };
 
 export const getPackages = (program: CommanderApi): IManifestDescriptor[] =>
@@ -21,3 +17,21 @@ export const getPackages = (program: CommanderApi): IManifestDescriptor[] =>
     data: readJsonSync(filePath),
     path: filePath
   }));
+
+const getYarnWorkspaces = (): string[] | null => {
+  const rootPackageJson = resolve(process.cwd(), 'package.json');
+  const pkgJson = readJsonSync(rootPackageJson, { throws: false });
+  if (pkgJson && pkgJson.workspaces && pkgJson.workspaces.length) {
+    return pkgJson.workspaces.map((glob: string) => join(glob, 'package.json'));
+  }
+  return null;
+};
+
+const getLernaPackages = (): string[] | null => {
+  const lernaPath = resolve(process.cwd(), 'lerna.json');
+  const lerna = readJsonSync(lernaPath, { throws: false });
+  if (lerna && lerna.packages && lerna.packages.length) {
+    return lerna.packages.map((glob: string) => join(glob, 'package.json'));
+  }
+  return null;
+};
