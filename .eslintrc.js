fd4f2578ae5ba19215c0bc51a4e503c47d820b13
Jamie Mason <jamie@foldleft.io>
2020-02-22 15:19:10 +0000

    chore(lint): migrate tslint → eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['*.spec.ts'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
