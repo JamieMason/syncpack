765376c17cf10422c349d76bb8335996046ea053
Jamie Mason <jamie@foldleft.io>
2023-06-03 08:30:39 +0100

    feat(cli): change output of lint command
    Closes #134

--- src/bin-lint/lint-cli.ts
+++ src/bin-lint/lint-cli.ts
@@ -1,10 +1,16 @@
 import { pipe } from 'tightrope/fn/pipe';
+import { lintSemverRanges } from '../bin-lint-semver-ranges/lint-semver-ranges';
+import { listMismatches } from '../bin-list-mismatches/list-mismatches';
 import type { CliConfig } from '../config/types';
 import { getContext } from '../get-context';
 import type { Disk } from '../lib/disk';
 import { exitIfInvalid } from '../lib/exit-if-invalid';
-import { lint } from './lint';
 
 export function lintCli(input: Partial<CliConfig>, disk: Disk): void {
-  pipe(getContext(input, disk), lint, exitIfInvalid);
+  pipe(
+    getContext(input, disk),
+    listMismatches,
+    lintSemverRanges,
+    exitIfInvalid,
+  );
 }
