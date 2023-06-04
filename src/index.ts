e369e57debd9ea4e781292b465f0b59275855134
Jamie Mason <jamie@foldleft.io>
2023-06-04 18:46:16 +0100

    [WIP]feat(node): add node.js api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-import type { RcConfig } from './config/types';
+import type { CliConfig, RcConfig } from './config/types';
+import type { Context } from './get-context';
+import { getContext } from './get-context';
+import type { AnySemverGroup } from './get-semver-groups';
+import { getSemverGroups } from './get-semver-groups';
+import type { AnyVersionGroup } from './get-version-groups';
+import { getVersionGroups } from './get-version-groups';
+import { effects } from './lib/effects';
 
 export type RcFile = Partial<RcConfig>;
+
+export const syncpack = {
+  getContext(input: Partial<CliConfig>): Context {
+    return getContext(input, effects);
+  },
+  getSemverGroups(input: Partial<CliConfig>): AnySemverGroup[] {
+    return getSemverGroups(getContext(input, effects));
+  },
+  getVersionGroups(input: Partial<CliConfig>): AnyVersionGroup[] {
+    return getVersionGroups(getContext(input, effects));
+  },
+};
