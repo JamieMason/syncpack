fee1f067026a0657ce9ac5dd4e73ee9ba9299b03
Jamie Mason <jamie@foldleft.io>
2025-02-02 14:34:12 +0000

    fix(npm): update dependencies

--- .syncpackrc.js
+++ .syncpackrc.js
@@ -8,8 +8,21 @@ const config = {
     },
   ],
   versionGroups: [
-    // v10 does not support Node 18
-    { dependencies: ['minimatch'], pinVersion: '9.0.5' },
+    {
+      label: 'v10 does not support Node 18',
+      dependencies: ['minimatch'],
+      pinVersion: '9.0.5',
+    },
+    {
+      label: 'v10 does not support Node 18',
+      dependencies: ['@release-it/conventional-changelog'],
+      pinVersion: '8.0.2',
+    },
+    {
+      label: 'v18 does not support Node 18',
+      dependencies: ['release-it'],
+      pinVersion: '17.11.0',
+    },
   ],
 };
 
