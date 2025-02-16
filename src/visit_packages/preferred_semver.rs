287d9c048196e4bf15c2c09282a2ecbc5d495f36
Jamie Mason <jamie@foldleft.io>
2025-02-16 19:24:46 +0000

    [WIP]

--- src/visit_packages/preferred_semver.rs
+++ src/visit_packages/preferred_semver.rs
@@ -1,9 +1,6 @@
 use {
   super::{L1, L10, L2, L3, L4, L5, L6, L7, L8, L9},
-  crate::{
-    context::Context,
-    specifier::semver_range::SemverRange,
-  },
+  crate::{context::Context, specifier::semver_range::SemverRange},
   log::debug,
 };
 
