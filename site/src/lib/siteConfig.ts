70d66ccede0295ccb3e8c78087c3bb8548f37818
Jamie Mason <jamie@foldleft.io>
2026-02-01 18:20:10 +0000

    chore(site): add unique meta descriptions

--- site/src/lib/siteConfig.ts
+++ site/src/lib/siteConfig.ts
@@ -8,11 +8,10 @@ export const siteConfig = {
   siteUrl: "https://jamiemason.github.io/syncpack",
   description: "Consistent dependency versions in large JavaScript Monorepos",
   softwareDescription:
-    "Command-line tool for managing dependency versions in JavaScript monorepos. Used by AWS, Cloudflare, DataDog, Electron, Microsoft, Vercel, and others.",
+    "CLI for managing dependency versions in JavaScript monorepos. Used by AWS, Cloudflare, DataDog, Electron, Microsoft, Vercel, and others.",
   logoUrl: "https://jamiemason.github.io/syncpack/logo.svg",
   npmUrl: "https://www.npmjs.com/package/syncpack",
   githubUrl: "https://github.com/JamieMason/syncpack",
-  // Author: Jamie Mason (Person)
   authorName: "Jamie Mason",
   authorUrl: "https://github.com/JamieMason",
   authorImage: "https://avatars.githubusercontent.com/u/320492?v=4",
